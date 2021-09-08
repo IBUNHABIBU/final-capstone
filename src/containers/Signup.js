120290d604b19446319cbd149e4603dbfd755256
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:18:04 +0300

    Remove the lint errors

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Signup = () => (
   <div>
-<form action="" method="post">
+    <form action="" method="post">
 
-<label htmlFor="name">
-  <input type="text" />
-</label>
-<label htmlFor="email">
-  <input type="email" name="" id="" />
-</label>
-<label htmlFor="password">
-  <input type="password" name="" id="" />
-</label>
-<label htmlFor="password">
-  <input type="password" name="" id="" />
-</label>
-</form>
+      <label htmlFor="name">
+        <input type="text" />
+      </label>
+      <label htmlFor="email">
+        <input type="email" name="" id="" />
+      </label>
+      <label htmlFor="password">
+        <input type="password" name="" id="" />
+      </label>
+      <label htmlFor="password">
+        <input type="password" name="" id="" />
+      </label>
+    </form>
   </div>
 );
 
