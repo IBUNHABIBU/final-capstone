4bb7aec7ea34250a2a4c7be4f5089f75a561346e
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:04:24 +0300

    Add lint configurations

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const Home = () => {
- return (
+const Home = () => (
   <div>
-   <h1>
-    Please register to rent a car
-   </h1>
-   <form action="" method="post">
-    
-    <label htmlFor="name">
-      <input type="text" />
-    </label>
-    <label htmlFor="email">
-      <input type="email" name="" id="" />
-    </label>
-    <label htmlFor="password">
-     <input type="password" name="" id="" />
-    </label>
-    <label htmlFor="password">
-     <input type="password" name="" id="" />
-    </label>
-   </form>
-  </div>
- )
-}
+    <h1>
+      Please register to rent a car
+    </h1>
+    <form action="" method="post">
 
-export default Home
+      <label htmlFor="name">
+        <input type="text" />
+      </label>
+      <label htmlFor="email">
+        <input type="email" name="" id="" />
+      </label>
+      <label htmlFor="password">
+        <input type="password" name="" id="" />
+      </label>
+      <label htmlFor="password">
+        <input type="password" name="" id="" />
+      </label>
+    </form>
+  </div>
+);
+
+export default Home;
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Signup = () => {
- return (
-  <div>
-   
-  </div>
- )
-}
+const Signup = () => (
+  <div />
+);
 
-export default Signup
+export default Signup;
