a1e3b7c8bf33ae55d1e2dbf3bbc1bbca73ca67ba
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 21:56:19 +0300

    Create signup form

---
+++ src/containers/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+ return (
+  <div>
+   <h1>
+    Please register to rent a car
+   </h1>
+   <form action="" method="post">
+    
+    <label htmlFor="name">
+      <input type="text" />
+    </label>
+    <label htmlFor="email">
+     <input type="email" name="" id="" />
+     </label>
+   </form>
+  </div>
+ )
+}
+
+export default Home
