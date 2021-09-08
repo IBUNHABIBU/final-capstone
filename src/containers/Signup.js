c2229ecaf0a8dd52d4c7d0dc0e7586b35bb379d3
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:20:04 +0300

    Create signup form

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -2,21 +2,19 @@ import React from 'react';
 
 const Signup = () => (
   <div>
-    <form action="" method="post">
-
-      <label htmlFor="name">
-        <input type="text" />
-      </label>
-      <label htmlFor="email">
-        <input type="email" name="" id="" />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="" id="" />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="" id="" />
-      </label>
-    </form>
+    <form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" />
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
   </div>
 );
 
