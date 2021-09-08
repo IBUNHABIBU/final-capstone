a53b1d66bfdac7b6fdbeeca9e93cbe8d61d9b745
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:45:00 +0300

    Remove the lint errors

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,18 +1,16 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const Signup = () => (
   <div>
     <form>
-      <div className="form-floating mb-3">
-        <label htmlFor="floatingInput">
-          floatingInput
-          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        </label>
+      <div className="form-floating mb-3 col-3">
+        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <label htmlFor="floatingInput">Email address</label>
       </div>
-      <div className="form-floating">
-        <label htmlFor="floatingPassword">floatingPassword
+      <div className="form-floating col-3">
+        <label htmlFor="floatingPassword">floatingPassword</label>
         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        </label>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
