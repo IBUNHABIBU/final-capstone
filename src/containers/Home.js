4597d543b263f32eeb113047aab78f40af1a32d0
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:14:00 +0300

    Create signup form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,25 +1,12 @@
 import React from 'react';
+import Signup from './Signup';
 
 const Home = () => (
   <div>
     <h1>
       Please register to rent a car
     </h1>
-    <form action="" method="post">
-
-      <label htmlFor="name">
-        <input type="text" />
-      </label>
-      <label htmlFor="email">
-        <input type="email" name="" id="" />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="" id="" />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="" id="" />
-      </label>
-    </form>
+    <Signup />
   </div>
 );
 
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Signup = () => (
-  <div />
+  <div>
+<form action="" method="post">
+
+<label htmlFor="name">
+  <input type="text" />
+</label>
+<label htmlFor="email">
+  <input type="email" name="" id="" />
+</label>
+<label htmlFor="password">
+  <input type="password" name="" id="" />
+</label>
+<label htmlFor="password">
+  <input type="password" name="" id="" />
+</label>
+</form>
+  </div>
 );
 
 export default Signup;
