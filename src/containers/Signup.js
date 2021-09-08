65ba8810285d1fb75ab99107b37ed418cca7a2c0
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:39:08 +0300

    Remove the lint errors

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -4,12 +4,15 @@ const Signup = () => (
   <div>
     <form>
       <div className="form-floating mb-3">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="Email">Email</label>
+        <label htmlFor="floatingInput">
+          floatingInput
+          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        </label>
       </div>
       <div className="form-floating">
+        <label htmlFor="floatingPassword">floatingPassword
         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="Password">Password</label>
+        </label>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
