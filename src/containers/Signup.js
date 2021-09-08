e9e1e03b215c45de7aa0cfb9a6a39b081937aa16
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-08 23:47:34 +0300

    Remove the lint errors

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -3,14 +3,18 @@ import React from 'react';
 
 const Signup = () => (
   <div>
-    <form>
+    <form className="container">
       <div className="form-floating mb-3 col-3">
         <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Email address</label>
+        <label htmlFor="floatingInput">Email</label>
       </div>
       <div className="form-floating mb-3 col-3">
         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">floatingPassword</label>
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      <div className="form-floating mb-3 col-3">
+        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <label htmlFor="floatingPassword">Confirm Password</label>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
