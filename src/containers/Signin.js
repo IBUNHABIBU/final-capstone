fa320c3b777f8dc95d6f64084c82289c38af0245
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-09 21:05:23 +0300

    Add signin form

--- src/containers/Signin.js
+++ src/containers/Signin.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 const Signin = () => {
  return (
-  <div>
-   
+  <div className="signup container">
+    <form className="form">
+      
+      <div className="form-floating mb-2 col-3">
+        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <label htmlFor="floatingInput">Email</label>
+      </div>
+      <div className="form-floating mb-2 col-3">
+        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      
+      <div className="form-floating mb-3 col-3">
+        <button type="submit" className="btn btn-primary col-12">Submit</button>
+      </div>
+      <p>
+        Already Member?
+        <span>  Login</span>
+      </p>
+    </form>
   </div>
  )
 }
