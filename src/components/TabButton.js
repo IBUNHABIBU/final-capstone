d3d30cc66a10c3bd86bb8df29aaf6b21dce7dba1
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-09 23:44:05 +0300

    Adding tabButton component

---
+++ src/components/TabButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TabButton = (props) => {
+ const {index, tab, content} = props;
+ return (
+  <div>
+    <button onClick={() => toggleTab(index)} className={tab === 1 ? 'nav-link active' : 'nav-link'} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">{content}</button>
+  </div>
+ )
+}
+
+export default TabButton
