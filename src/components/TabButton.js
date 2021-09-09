a555a936a681552da17d6bc3d61cf093560b5752
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-10 00:01:51 +0300

    Add proptypes

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 
 const TabButton = (props) => {
-  const { index, clasVar, content } = props;
+  const { index, btnNumber, content } = props;
   const [tab, setTab] = useState(1);
   const toggleTab = (index) => {
     setTab(index);
   };
   return (
     <div>
-      <button onClick={() => toggleTab(index)} className={clasVar === 1 ? 'nav-link active' : 'nav-link'} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">{content}</button>
+      <button onClick={() => toggleTab(index)} className={ tab=== btnNumber ? 'nav-link active' : 'nav-link'} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">{content}</button>
     </div>
   );
 };
-
+TabButton.propTypes = {
+ ind
+}
 export default TabButton;
