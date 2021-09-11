c10b6940fd55e4114666f163d7fb41a493ab18f0
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-11 20:05:15 +0300

    Define handleChange method

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,47 +1,54 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [user, setUser] = useState('')
   const handleSubmit = (e) => {
-    console.log("submited");
+    console.log('submited');
     e.preventDefault();
-  }
+  };
   const handleChange = (e) => {
-    console.log("onChange", e.target.value);
+    // console.log('onChange', e.target.value);
+    setUser({ name: })
     e.preventDefault();
-  }
+  };
   return (
-  <div className="signup container">
-    <form className="form" onSubmit={handleSubmit}>
-      <div className="form-floating mb-2 col-3">
-        <input type="text"
-        className="form-control"
-        onChange={handleChange}
-        id="floatingInput"
-        placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Username</label>
-      </div>
-      <div className="form-floating mb-2 col-3">
-        <input type="email" onChange={handleChange} className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Email</label>
-      </div>
-      <div className="form-floating mb-2 col-3">
-        <input type="password" onChange={handleChange} className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">Password</label>
-      </div>
-      <div className="form-floating mb-3 col-3">
-        <input type="password" onChange={handleChange} className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">Confirm Password</label>
-      </div>
-      <div className="form-floating mb-3 col-3">
-        <button type="submit" className="btn btn-primary col-12">Sign Up</button>
-      </div>
-      <p>
-        Already Member?
-        <span>  Login</span>
-      </p>
-    </form>
-  </div>
-)};
+    <div className="signup container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-floating mb-2 col-3">
+          <input
+            type="text"
+            className="form-control"
+            name = "name"
+            onChange={handleChange}
+            value = {user.name}
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInput">Username</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="email" name="email" value={user.email} onChange={handleChange} className="form-control" id="floatingInput" placeholder="name@example.com" />
+          <label htmlFor="floatingInput">Email</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="password" value={user.password} onChange={handleChange} className="form-control" id="floatingPassword" placeholder="Password" />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+        <div className="form-floating mb-3 col-3">
+          <input type="password" value={user.password_confirmation} onChange={handleChange} className="form-control" id="floatingPasswordConfirm" placeholder="Password" />
+          <label htmlFor="floatingPasswordconfirm">Confirm Password</label>
+        </div>
+        <div className="form-floating mb-3 col-3">
+          <button type="submit" className="btn btn-primary col-12">Sign Up</button>
+        </div>
+        <p>
+          Already Member?
+          <span>  Login</span>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Signup;
