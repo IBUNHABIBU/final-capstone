24d8f07501b6e7d1e1d5411e6f0df5461acb9df3
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-12 00:41:56 +0300

    Implement login functionality

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,23 +1,27 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { useState } from 'react';
 
-const Login = () => (
-  <div className="signup container">
-    <form className="form">
-      <div className="form-floating mb-2 col-3">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Email</label>
-      </div>
-      <div className="form-floating mb-2 col-3">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">Password</label>
-      </div>
+const Login = () => {
+  const[logStatus, setLogStatus] = useState('NOT_LOGGED_IN');
+  const { register, handleSubmit } = useForm();
+  return(
+  <div className="Login container">
+    <form className="form" onSubmit={handleSubmit}>
+       <div className="form-floating mb-2 col-3">
+          <input type="email" name="email" {...register('email')} className="form-control" id="floatingInputEmail" placeholder="name@example.com" />
+          <label htmlFor="floatingInputEmail">Email</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="password" name="password" {...register('password')} className="form-control" id="floatingPassword" placeholder="Password" />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
 
       <div className="form-floating mb-3 col-3">
         <button type="submit" className="btn btn-primary col-12">Login</button>
       </div>
     </form>
   </div>
-);
+)};
 
 export default Login;
