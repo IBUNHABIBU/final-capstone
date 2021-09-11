b56c07528ffd40a4e2023ed19feae4668ea4b007
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-12 00:22:45 +0300

    Define onSubmit method

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,24 +1,18 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-props-no-spreading */
 import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const Signup = () => {
   const { register, handleSubmit } = useForm();
-  const [user, setUser] = useState(
-    {
-      name: '', email: '', password: '', password_confirmation: '',
-    },
-  );
   const onSubmit = (data) => {
-    console.log(data);
-    console.log(user.email);
     axios.post('http://localhost:3001/users', {
       user: {
-        name: user.name,
-        email: user.email,
-        password: user.password,
-        password_confirmation: user.password_confirmation,
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        password_confirmation: data.password_confirmation,
       },
     },
     { withCredentials: true }).then((response) => {
@@ -27,32 +21,31 @@ const Signup = () => {
       console.log('Error', error);
     });
   };
-  
+
   return (
     <div className="signup container">
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-floating mb-2 col-3">
           <input
             type="text"
             className="form-control"
             name="name"
-            
-            ref={register}
+            {...register('name')}
             id="floatingInput"
             placeholder="name@example.com"
           />
           <label htmlFor="floatingInput">Username</label>
         </div>
         <div className="form-floating mb-2 col-3">
-          <input type="email" name="email" ref={register}  className="form-control" id="floatingInputEmail" placeholder="name@example.com" />
+          <input type="email" name="email" {...register('email')} className="form-control" id="floatingInputEmail" placeholder="name@example.com" />
           <label htmlFor="floatingInputEmail">Email</label>
         </div>
         <div className="form-floating mb-2 col-3">
-          <input type="password" name="password" ref={register}  className="form-control" id="floatingPassword" placeholder="Password" />
+          <input type="password" name="password" {...register('password')} className="form-control" id="floatingPassword" placeholder="Password" />
           <label htmlFor="floatingPassword">Password</label>
         </div>
         <div className="form-floating mb-3 col-3">
-          <input type="password" name="password_confirmation" ref={register}  className="form-control" id="floatingPasswordConfirm" placeholder="Password" />
+          <input type="password" name="password_confirmation" {...register('password_confirmation')} className="form-control" id="floatingPasswordConfirm" placeholder="Password" />
           <label htmlFor="floatingPasswordconfirm">Confirm Password</label>
         </div>
         <div className="form-floating mb-3 col-3">
