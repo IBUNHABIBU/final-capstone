21a48fac471b79d13f22e2fe9b07194e5732de4b
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-12 20:58:17 +0300

    Styling the navbar

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -11,26 +11,23 @@ const TabButton = () => {
     setTab(index);
   };
   return (
-    <div>
+    <div className="sidenav">
       <div className="d-flex align-items-start">
         <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-          <Link to="/">
+          <Link to="/" className="link">
             <button onClick={() => toggleTab(1)} className={tab === 1 ? 'nav-link active' : 'nav-link'} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
           </Link>
-          <Link to="/Models">
+          <Link to="/Models" className="link">
             <button onClick={() => toggleTab(2)} className={tab === 2 ? 'nav-link active' : 'nav-link'} id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Models</button>
           </Link>
-          <Link to="lifestyle">
+          <Link to="lifestyle" className="link">
             <button onClick={() => toggleTab(3)} className={tab === 3 ? 'nav-link active' : 'nav-link'} id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Shop</button>
           </Link>
-          <Link to="booking">
+          <Link to="booking" className="link">
 
             <button onClick={() => toggleTab(4)} className={tab === 4 ? 'nav-link active' : 'nav-link'} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Test Drive</button>
           </Link>
 
-        </div>
-        <div className="tab-content" id="v-pills-tabContent">
-          {/*  */}
         </div>
       </div>
     </div>
