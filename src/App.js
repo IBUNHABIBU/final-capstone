1ab5ec17915ef7bae411d838e454231735669dc8
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-12 20:16:25 +0300

    Remove the lint errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import Home from './containers/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import Models from './containers/Models';
 import Booking from './containers/Booking';
 import TabButton from './components/TabButton';
+
 function App() {
   return (
     <div className="App">
       <Router>
-       <TabButton />
-       <Switch>
-         <Route path="/" exact component={Home} />
-         <Route path="/login" exact component={Login} />
-         <Route path="/signup" exact component={Signup} />
-         <Route path="/models" exact component={Models} />
-         <Route path="/booking" exact component={Booking} />
-       </Switch>
+        <TabButton />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/models" exact component={Models} />
+          <Route path="/booking" exact component={Booking} />
+        </Switch>
       </Router>
     </div>
   );
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { useState } from 'react';
 
 const Login = () => {
-  const[logStatus, setLogStatus] = useState('NOT_LOGGED_IN');
+  const [logStatus, setLogStatus] = useState('NOT_LOGGED_IN');
   const { register, handleSubmit } = useForm();
-  return(
-  <div className="Login container">
-    <form className="form" onSubmit={handleSubmit}>
-       <div className="form-floating mb-2 col-3">
+  return (
+    <div className="Login container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-floating mb-2 col-3">
           <input type="email" name="email" {...register('email')} className="form-control" id="floatingInputEmail" placeholder="name@example.com" />
           <label htmlFor="floatingInputEmail">Email</label>
         </div>
@@ -17,11 +17,12 @@ const Login = () => {
           <label htmlFor="floatingPassword">Password</label>
         </div>
 
-      <div className="form-floating mb-3 col-3">
-        <button type="submit" className="btn btn-primary col-12">Login</button>
-      </div>
-    </form>
-  </div>
-)};
+        <div className="form-floating mb-3 col-3">
+          <button type="submit" className="btn btn-primary col-12">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
