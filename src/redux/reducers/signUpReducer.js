fce198dc4412ea6f9dbe32ff2bcdbc67fe848065
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-13 15:35:34 +0300

    Implement the SignupReducer

---
+++ src/redux/reducers/signUpReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+ logInStatus: 'NOT_LOGGED_IN',
+}
+
+const signUpReducer = (state= initialState, {type, payload}) => {
+   switch (type) {
+    case value:
+     
+     break;
+   
+    default:
+     break;
+   }
+}
