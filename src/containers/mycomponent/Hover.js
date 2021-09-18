55d21e10e0b53f1913ffa87cfda3bdfc1dcb539d
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-18 19:35:33 +0300

    Render props

--- src/containers/mycomponent/Hover.js
+++ src/containers/mycomponent/Hover.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Hover extends Component {
- render() {
-  constructor(props) {
-   super(props)
-  
-   this.state = {
-     
-   }
+ constructor(props) {
+  super(props)
+ 
+  this.state = {
+    
   }
-  
+ }
+ 
+ incrementCount = () => {
+  this.setState((prevState) => ({ count: prevState.count + 1 }));
+}
+ render() {
   return (
    <div>
     <h2 onMouseOver={incrementCounter} onFocus={incrementCounter}>Hovered {count} times</h2>
