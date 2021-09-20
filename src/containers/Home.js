2e93666e68707bae3f16d8a64bdb321c1d36cec9
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-20 09:29:43 +0300

    Check Login status

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,19 @@
+import axios from 'axios';
 import React from 'react';
 
-const Home = () => (
+const Home = () => {
+  const checkLoginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', {
+      withCredentials:true,
+    }).then(response => {
+      console.log(response);
+    })
+  }
+  return (
   <div>
     <h1>
       Logo
     </h1>
   </div>
-);
+)};
 export default Home;
