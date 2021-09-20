9f45fb2c9b64898484436ee2bbe2b102f99962bc
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-20 07:42:45 +0300

    Adding logout button

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import ClickCounter from './ClickCounter';
-import HoverCounter from './HoverCounter';
-import CounterComponent from './mycomponent/CounterComponent';
-import CounterTwo from './mycomponent/CounterTwo';
-import Hover from './mycomponent/Hover';
 
 const Home = () => (
   <div>
     <h1>
       Logo
     </h1>
-    <ClickCounter name="Juma" />
-    <HoverCounter />
-    <CounterComponent>
-      {
-          (count, incrementCount) => <CounterTwo count={count} incrementCount={incrementCount} />
-        }
-    </CounterComponent>
-    <CounterComponent>
-      {
-          (count, incrementCount) => <Hover count={count} incrementCount={incrementCount} />
-        }
-    </CounterComponent>
-
+    
   </div>
 );
 export default Home;
