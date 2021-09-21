daedb6bcd3c2339b86ccbef4e8ac661c37eef2bd
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-21 15:14:28 +0300

    Implement the booking component

--- src/containers/Booking.js
+++ src/containers/Booking.js
@@ -1,21 +1,54 @@
 import React from 'react';
 
-const Booking = () => (
-  <div>
-    <div className="card text-center">
-      <div className="card-header">
-        Featured
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <button type="submit" className="btn btn-primary">Go somewhere</button>
-      </div>
-      <div className="card-footer text-muted">
-        2 days ago
-      </div>
+const Booking = () => {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const onSubmit = (data) => {
+    axios.post('http://localhost:3001/api/v1/cars', {
+      car: {
+        color: data.color,
+        engine: data.engine,
+        year: data.year,
+      },
+    },
+    { withCredentials: true }).then((response) => {
+      console.log('Post', response.data);
+      if (response.data.status === 'created') {
+        dispatch(setUser());
+      }
+    }).catch((error) => {
+      console.log('Error', error);
+    });
+  };
+
+  return (
+    <div className="signup container">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-floating mb-2 col-3">
+          <input
+            type="text"
+            className="form-control"
+            name="color"
+            {...register('color')}
+            id="floatingInput"
+            placeholder="Car color"
+          />
+          <label htmlFor="floatingInput">Color</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="text" name="engine" {...register('engine')} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
+          <label htmlFor="floatingInputEmail">Engine</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="text" name="year" {...register('year')} className="form-control" id="floatingPassword" placeholder="Enter Year" />
+          <label htmlFor="floatingPassword">Year</label>
+        </div>
+        <div className="form-floating mb-3 col-3">
+          <button type="submit" className="btn btn-primary col-12">Add Car</button>
+        </div>
+      </form>
     </div>
-  </div>
-);
+  );
+  };
 
 export default Booking;
