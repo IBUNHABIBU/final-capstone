7ffb601ff4f731ce23745e873c8bb1a6d27ae57b
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-21 13:06:13 +0300

    Implement fetch methods

--- src/redux/actions/fetch.js
+++ src/redux/actions/fetch.js
@@ -1,7 +1,11 @@
-export const fetchCars = () => async (dispatch) => {
+import axios from "axios";
+import { setUser } from "./actions";
 
+export const fetchCars = () => async (dispatch) => {
+  const response = await axios.get('http://localhost:3001/api/v1/cars');
+  dispatch(setUser(response.data));
 };
 
-export const fetchBooking = () => async (dispatch) => {
+export const fetchBooking = () => {
 
 };
