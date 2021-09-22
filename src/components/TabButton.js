4546d26fa314d372d8bb3f7a0c334c903a807bf6
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-22 22:14:14 +0300

    Implement admin dashboard

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -38,20 +38,18 @@ const TabButton = () => {
 
           </Link>
           <button onClick={() => toggleTab(3)} className={tab === 3 ? 'nav-link active' : 'nav-link'} id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Shop</button>
-          <Link to="/booking" className="link">
-
-            <button onClick={() => toggleTab(4)} className={tab === 4 ? 'nav-link active' : 'nav-link'} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Test Drive</button>
-          </Link>
-
+         
           <hr />
           {userLoggedIn ? (
             <div>
               <div>
                 {(stayLoggedIn.user.admin || signin.user.admin)
-                  ? (<Link to="/booking" className="link">
+                  ? (
+                    <Link to="/booking" className="link">
 
-                  <button onClick={() => toggleTab(4)} className={tab === 4 ? 'nav-link active' : 'nav-link'} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Add Car</button>
-                </Link>) : (<div>Not Admin</div>) }
+                      <button onClick={() => toggleTab(4)} className={tab === 4 ? 'nav-link active' : 'nav-link'} id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Add Car</button>
+                    </Link>
+                  ) : (<div>Not Admin</div>) }
                 {' '}
               </div>
               <p>
