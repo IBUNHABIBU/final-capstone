a1a188f882491f5d20a2960695c235afc38d1d7a
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-23 21:16:59 +0300

    Remove unused variables

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import signUpReducer from './signUpReducer';
-// import loginReducer from './loginReducer';
 
 const reducers = combineReducers({
   register: signUpReducer,
-  // login: loginReducer,
 });
 
 export default reducers;
