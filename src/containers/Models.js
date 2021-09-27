f6c67e220e1c4a2ad66a4faf6164856791adb561
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-27 14:51:02 +0300

    Remove the lint errors

--- src/containers/Models.js
+++ src/containers/Models.js
@@ -1,8 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCars } from '../redux/actions/fetch';
 import CarList from './CarList';
+import { addCar } from '../redux/actions/actions';
 
 const Models = () => {
   const dispatch = useDispatch();
@@ -54,34 +58,34 @@ const Models = () => {
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="close" />
                       </div>
                       <div className="modal-body">
-                      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-floating mb-2 col-3">
-          <input
-            type="text"
-            className="form-control"
-            name="color"
-            {...register('color')}
-            id="floatingInput"
-            placeholder="Car color"
-          />
-          <label htmlFor="floatingInput">Color</label>
-        </div>
-        <div className="form-floating mb-2 col-3">
-          <input type="text" name="engine" {...register('engine')} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
-          <label htmlFor="floatingInputEmail">Engine</label>
-        </div>
-        <div className="form-floating mb-2 col-3">
-          <input type="text" name="year" {...register('year')} className="form-control" id="floatingPassword" placeholder="Enter Year" />
-          <label htmlFor="floatingPassword">Year</label>
-        </div>
-        <div className="form-floating mb-2 col-3">
-          <input type="file" name="image" {...register('image')} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
-          <label htmlFor="floatingInputEmail">Image</label>
-        </div>
-        <div className="form-floating mb-3 col-3">
-          <button type="submit" className="btn btn-primary col-12">Add Car</button>
-        </div>
-      </form>
+                        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                          <div className="form-floating mb-2 col-3">
+                            <input
+                              type="text"
+                              className="form-control"
+                              name="color"
+                              {...register('color')}
+                              id="floatingInput"
+                              placeholder="Car color"
+                            />
+                            <label htmlFor="floatingInput">Color</label>
+                          </div>
+                          <div className="form-floating mb-2 col-3">
+                            <input type="text" name="engine" {...register('engine')} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
+                            <label htmlFor="floatingInputEmail">Engine</label>
+                          </div>
+                          <div className="form-floating mb-2 col-3">
+                            <input type="text" name="year" {...register('year')} className="form-control" id="floatingPassword" placeholder="Enter Year" />
+                            <label htmlFor="floatingPassword">Year</label>
+                          </div>
+                          <div className="form-floating mb-2 col-3">
+                            <input type="file" name="image" {...register('image')} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
+                            <label htmlFor="floatingInputEmail">Image</label>
+                          </div>
+                          <div className="form-floating mb-3 col-3">
+                            <button type="submit" className="btn btn-primary col-12">Add Car</button>
+                          </div>
+                        </form>
                       </div>
                     </div>
                   </div>
