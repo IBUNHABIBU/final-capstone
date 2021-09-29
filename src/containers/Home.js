c36a8b741c40abf2f77a3ea5775b0e750e40048e
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-29 22:03:43 +0300

    Modif the home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Home = () => (
   <div className="container m-5 model">
-
+    <div className="arrows left" />
+    <div className="arrows right" />
   </div>
 );
 export default Home;
