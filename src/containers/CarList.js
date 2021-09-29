9e46260ea5771018e5e83409d29caf7846aca46a
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-29 12:46:25 +0300

    Styling the carlist

--- src/containers/CarList.js
+++ src/containers/CarList.js
@@ -18,32 +18,34 @@ const CarList = () => {
     );
   }
   const displayCars = (
-    <div className="p-2 carlist">
-      <div className="row">
-        {
-              cars.map((car) => {
-                const {
-                  id, color, engine, year,
-                } = car;
-                return (
-                  <div className=" col-6 col-lg-3 col-md-6 p-1" key={id}>
-                    <Link to={`/category/${id}`} className="link">
-                      <div className="card ">
-                        {/* <img src={mainImage} className="card-img-top" alt="Thumb" /> */}
-                        <div className="card-body">
-                          <h5 className="card-text">
-                            {' '}
-                            {color}
-                          </h5>
-                          <p>{engine}</p>
-                          <p>{year}</p>
+    <div className="car-container">
+      <div className="p-2 carlist">
+        <div className="row">
+          {
+                cars.map((car) => {
+                  const {
+                    id, color, engine, year,
+                  } = car;
+                  return (
+                    <div className=" col-6 col-lg-3 col-md-6 p-1" key={id}>
+                      <Link to={`/category/${id}`} className="link">
+                        <div className="card ">
+                          {/* <img src={mainImage} className="card-img-top" alt="Thumb" /> */}
+                          <div className="card-body">
+                            <h5 className="card-text">
+                              {' '}
+                              {color}
+                            </h5>
+                            <p>{engine}</p>
+                            <p>{year}</p>
+                          </div>
                         </div>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })
-        }
+                      </Link>
+                    </div>
+                  );
+                })
+          }
+        </div>
       </div>
     </div>
   );
