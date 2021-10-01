7129bde75615fda159614a8b0e9063f6357a6706
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-01 12:34:07 +0300

    Styling the home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -20,7 +20,16 @@ const Home = () => {
       <div className="container m-5 model">
 
         <Carousel breakPoints={breakPoints}>
-          <div className="slides">One</div>
+          <div className="slides">
+          <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          </div>
           <div className="slides">Two</div>
           <div className="slides">Three</div>
           <div className="slides">Four</div>
