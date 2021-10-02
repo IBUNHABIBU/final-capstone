f60152d99a379dc8836bcf50afddb00471cd99f1
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-02 22:41:27 +0300

    Add slide images

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'react-elastic-carousel';
+import { Link } from 'react-router-dom';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -21,22 +22,85 @@ const Home = () => {
 
         <Carousel breakPoints={breakPoints}>
           <div className="slides">
-          <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+            <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
           </div>
-          <div className="slides">Two</div>
-          <div className="slides">Three</div>
-          <div className="slides">Four</div>
-          <div className="slides">Five</div>
-          <div className="slides">six</div>
-          <div className="slides">Seven</div>
-          <div className="slides">Eight</div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
+          <div className="slides">
+          <div className="card">
+              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <div className="card-body">
+                <h5 className="card-title">Cadillac Escalade</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+          </div>
         </Carousel>
 
       </div>
