53129263985795b4147f1bf5b4baf44c39b706a8
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-02 22:57:33 +0300

    Add slide images

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -26,17 +26,17 @@ const Home = () => {
               <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
               <div className="card-body">
                 <h5 className="card-title">Cadillac Escalade</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <p className="card-text">v8 of the cardcontent.</p>
                 <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
               </div>
             </div>
           </div>
           <div className="slides">
             <div className="card">
-              <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Cadillac/Cadillac-Escalade/3027/1560320021838/front-left-side-47.jpg?tr=w-360" className="card-img-top" alt="cadillac" />
+              <img src="https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/media/51/2012-cadillac-cts-inline-678-photo-623583-s-original.jpg?resize=768:*" className="card-img-top" alt="cadillac" />
               <div className="card-body">
-                <h5 className="card-title">Cadillac Escalade</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                <h5 className="card-title">Cadillac CTS</h5>
+                <p className="card-text">V6.</p>
                 <Link to="/cars/id" className="btn btn-primary">Go somewhere</Link>
               </div>
             </div>
