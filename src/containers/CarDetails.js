cad9e23a97facb5a918b984060d2691af317a120
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-03 00:47:03 +0300

    Styling the details page

--- src/containers/CarDetails.js
+++ src/containers/CarDetails.js
@@ -14,20 +14,12 @@ const CarDetails = () => (
         <table className="table table-striped">
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
               <td>@mdo</td>
             </tr>
             <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
               <td>@fat</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
-              <td colSpan="2">Larry the Bird</td>
               <td>@twitter</td>
             </tr>
           </tbody>
