c90c90c885335cfdb995f0165f7bbe34295b4281
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-04 09:57:03 +0300

    Styling the details page

--- src/containers/CarDetails.js
+++ src/containers/CarDetails.js
@@ -7,27 +7,16 @@ const CarDetails = () => (
     </div>
     <div className="details-section">
       <div className="details-header">
-        <p className="lead">Cadillac Escallade</p>
-        <p>Rent for low price</p>
+        <p className="display-6">Cadillac Escallade</p>
+        <p className="lead">Rent for low price</p>
       </div>
       <div className="details">
-        <table className="table table-striped">
-          <tbody>
-            <tr>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>@twitter</td>
-            </tr>
-            <tr>
-              <td>@twitter</td>
-            </tr>
-
-          </tbody>
-        </table>
+        <ul>
+          <li>ab</li>
+          <li>cd</li>
+          <li>ef</li>
+          <li>gh</li>
+        </ul>
       </div>
     </div>
   </div>
