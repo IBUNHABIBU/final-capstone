9ce638a940b0f2a12585e2ddf585844675da253f
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-04 10:26:16 +0300

    Fetch the car details

--- src/containers/CarDetails.js
+++ src/containers/CarDetails.js
@@ -3,34 +3,34 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 
 const CarDetails = () => {
-
-  const {id} = useParams();
+  const { id } = useParams();
   useEffect(() => {
-   axios.get(`http://localhost:3001/api/v1/cars/${id}`)
-   .then(res => {
-     console.log(res);
-   })
-  }, [id])
-  return(
-  <div className="details-container">
-    <div className="details-image">
-      <img src="" alt="" />
-    </div>
-    <div className="details-section">
-      <div className="details-header">
-        <p className="display-6">Cadillac Escallade</p>
-        <p className="lead">Rent for low price</p>
+    axios.get(`http://localhost:3001/api/v1/cars/${id}`)
+      .then((res) => {
+        console.log(res);
+      });
+  }, [id]);
+  return (
+    <div className="details-container">
+      <div className="details-image">
+        <img src="" alt="" />
       </div>
-      <div className="details">
-        <ul>
-          <li>ab</li>
-          <li>cd</li>
-          <li>ef</li>
-          <li>gh</li>
-        </ul>
+      <div className="details-section">
+        <div className="details-header">
+          <p className="display-6">Cadillac Escallade</p>
+          <p className="lead">Rent for low price</p>
+        </div>
+        <div className="details">
+          <ul>
+            <li>ab</li>
+            <li>cd</li>
+            <li>ef</li>
+            <li>gh</li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default CarDetails;
