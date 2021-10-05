b1a39c873d0da6de407404c72455e4bff8f223f3
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-05 17:07:00 +0300

    Add booking form

--- src/containers/CarDetails.js
+++ src/containers/CarDetails.js
@@ -12,6 +12,24 @@ const CarDetails = () => {
   }, [id]);
 
   const { color, engine, year } = details;
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const onSubmit = (data) => {
+    axios.post('http://localhost:3001/api/v1/bookings', {
+      booking: {
+        name: data.name,
+        model: data.model,
+        pickup: data.pickup,
+        return: data.return,
+        location: data.location,
+      },
+    },
+    { withCredentials: true }).then((response) => {
+      if (response.data.status === 'created') {
+        dispatch(createCarBooking(response.data));
+      }
+    });
+  };
   return (
     <div className="details-container">
       <div className="details-image">
@@ -39,49 +57,60 @@ const CarDetails = () => {
             </li>
           </ul>
         </div>
-        <button type="button" className="btn btn-primary">Book a ride</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#myModal"
+        >
+          Book a ride
+        </button>
         <div className="modal" id="myModal">
-                  <div className="modal-dialog">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title">
-                          Add new car
-                        </h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="close" />
-                      </div>
-                      <div className="modal-body">
-                        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                          <div className="form-floating mb-2 col-10">
-                            <input
-                              type="text"
-                              className="form-control"
-                              name="color"
-                              {...register('color', { required: true })}
-                              id="floatingInput"
-                              placeholder="Car color"
-                            />
-                            <label htmlFor="floatingInput">Color</label>
-                          </div>
-                          <div className="form-floating mb-2 col-10">
-                            <input type="text" name="engine" {...register('engine', { required: true })} className="form-control" id="floatingInputEmail" placeholder="Enter engine type" />
-                            <label htmlFor="floatingInputEmail">Engine</label>
-                          </div>
-                          <div className="form-floating mb-2 col-10">
-                            <input type="text" name="year" {...register('year', { required: true })} className="form-control" id="floatingPassword" placeholder="Enter Year" />
-                            <label htmlFor="floatingPassword">Year</label>
-                          </div>
-                          <div className="form-floating mb-2 col-10">
-                            <input type="text" name="image" {...register('image', { required: true })} className="form-control" id="floatingInputImage" placeholder="Enter engine type" accept="image/*" />
-                            <label htmlFor="floatingInputImage">Image</label>
-                          </div>
-                          <div className="form-floating mb-3 col-10">
-                            <button type="submit" className="btn btn-primary col-10">Add Car</button>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">
+                  Add new car
+                </h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="close" />
+              </div>
+              <div className="modal-body">
+              <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-floating mb-2 col-3">
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            {...register('name')}
+            id="floatingInput"
+            placeholder="Car color"
+          />
+          <label htmlFor="floatingInput">Name</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="text" name="model" {...register('model')} className="form-control" id="floatingInputEmail" placeholder="Enter model type" />
+          <label htmlFor="floatingInputEmail">model</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="datetime-local" name="pickup" {...register('pickup')} className="form-control" id="floatingInputEmail" placeholder="Enter pickup date" />
+          <label htmlFor="floatingInputEmail">Pickup Date</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="datetime-local" name="return" {...register('return')} className="form-control" id="floatingInputEmail" placeholder="Enter return date" />
+          <label htmlFor="floatingInputEmail">Return Date</label>
+        </div>
+        <div className="form-floating mb-2 col-3">
+          <input type="text" name="location" {...register('location')} className="form-control" id="floatingPassword" placeholder="Enter location" />
+          <label htmlFor="floatingPassword">Location</label>
+        </div>
+        <div className="form-floating mb-3 col-3">
+          <button type="submit" className="btn btn-primary col-12">Book ride</button>
+        </div>
+      </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
