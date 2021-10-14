4e6cea9498f905689e18f3dfa403f31945f91b00
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-14 18:24:45 +0300

    Styling the form success message

--- src/containers/Models.js
+++ src/containers/Models.js
@@ -60,10 +60,8 @@ const Models = () => {
                   <div className="modal-dialog">
                     <div className="modal-content">
                       <div className="modal-header">
-
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="close" >
-                        {message && <span className="alert alert-success col-12" role="alert">{message}</span>}
-                        </button>
+                        {message && <div className="alert alert-success col-12" role="alert">{message}</div>}
+                        <button type="button" className="btn-close alert-success close-button" data-bs-dismiss="modal" aria-label="close" />
                       </div>
                       <div className="modal-body">
                         <form className="form" onSubmit={handleSubmit(onSubmit)}>
