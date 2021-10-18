32731f77afdb375bc10c01e92e33ca35d783af54
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-18 12:43:52 +0300

    Add Base url

--- src/redux/actions/fetch.js
+++ src/redux/actions/fetch.js
@@ -3,37 +3,38 @@ import {
   addCar, setUser, bookCar, selectCar,
 } from './actions';
 
+const urlBase = 'https://final-capstone-rails-api.herokuapp.com/'
 export const fetchCars = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3001/api/v1/cars');
+  const response = await axios.get(`${urlBase}/api/v1/cars`);
   dispatch(addCar(response.data));
 };
 
 export const fetchBookings = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3001/api/v1/bookings');
+  const response = await axios.get(`${urlBase}/api/v1/bookings`);
   dispatch(bookCar(response.data));
 };
 
 export const checkLoginStatus = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3001/logged_in', {
+  const response = await axios.get(`${urlBase}/logged_in`, {
     withCredentials: true,
   });
   dispatch(setUser(response.data));
 };
 
 export const fetchDetails = (id) => async (dispatch) => {
-  const response = await axios.get(`http://localhost:3001/api/v1/cars/${id}`);
+  const response = await axios.get(`${urlBase}/api/v1/cars/${id}`);
   dispatch(selectCar(response.data));
 };
 
 export const createCarBooking = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3001/api/v1/bookings', {
+  const response = await axios.get(`${urlBase}/api/v1/bookings`, {
     withCredentials: true,
   });
   dispatch(bookCar(response.data));
 };
 
 export const createCar = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3001/api/v1/cars', {
+  const response = await axios.get(`${urlBase}/api/v1/cars`, {
     withCredentials: true,
   });
   dispatch(addCar(response.data));
