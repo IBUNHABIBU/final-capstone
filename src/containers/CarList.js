df1b58e556ad42ecc538713b872822d4de8dd63a
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-23 19:11:18 +0300

    Remove lint errors

--- src/containers/CarList.js
+++ src/containers/CarList.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import uuid from 'react-uuid';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Loading from '../components/Loading';
-import { fetchCars } from '../redux/actions/fetch';
 
 const CarList = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCars());
-  }, []);
-
   const cars = useSelector((state) => state.car);
   if (cars === undefined) {
     return (
@@ -32,17 +26,17 @@ const CarList = () => {
                         <img alt="car" src="https://images.dealer.com/ddc/vehicles/2021/CADILLAC/Escalade%20ESV/SUV/color/Black%20Raven-GBA-5,5,7-640-en_US.jpg" />
                         <div className="card-body bg-light">
                           <h5>Model Type: new </h5>
-                          <h5 className="card-title" key={uuid()}>
+                          <h5 className="card-title">
                             Color:
                             {' '}
                             {color}
                           </h5>
-                          <p className="card-text" key={uuid()}>
+                          <p className="card-text">
                             Engine Type:
                             {' '}
                             {engine}
                           </p>
-                          <Link to={`/cars/${id}`} className="btn btn-primary" key={uuid()}>see details</Link>
+                          <Link to={`/cars/${id}`} className="btn btn-primary">see details</Link>
                         </div>
                       </div>
                     </div>
