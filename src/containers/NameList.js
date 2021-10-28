3f4713b6ed76e66c7c87ceb8749bb2e62c7132bd
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-28 19:27:37 +0300

    Change the NameList code, propTypes Validation

--- src/containers/NameList.js
+++ src/containers/NameList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 const NameList = ({ car }) => (
   <div>
     <p>{car.color}</p>
@@ -9,8 +10,8 @@ const NameList = ({ car }) => (
 
 NameList.propTypes = {
   car: PropTypes.shape({
-   color: PropTypes.string.isRequired,
-   engine: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    engine: PropTypes.string.isRequired,
   }).isRequired,
-}
+};
 export default NameList;
