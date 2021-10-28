b675bed31198c4f5616d1a3d91c44023965d7a63
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-28 19:21:57 +0300

    Change the CarList code, propTypes Validation

---
+++ src/containers/NameList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const NameList = ({ car }) => (
+  <div>
+    <p>{car.color}</p>
+    <p>{car.engine}</p>
+  </div>
+);
+
+NameList.propTypes = {
+  car: PropTypes.shape({
+   color: PropTypes.string.isRequired,
+   engine: PropTypes.string.isRequired,
+  })
+}
+export default NameList;
