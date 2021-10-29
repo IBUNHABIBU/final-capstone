c179127efa6a0233d885c04daf3ba60e05f81567
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-29 08:14:26 +0300

    Fix the failing test

--- src/redux/actions/fetch.js
+++ src/redux/actions/fetch.js
@@ -3,9 +3,7 @@ import {
   addCar, setUser, bookCar, selectCar,
 } from './actions';
 
-// export const urlBase = 'https://finalcapstone-rails-api.herokuapp.com';
-
-export const urlBase = 'http://localhost:3001';
+export const urlBase = 'https://finalcapstone-rails-api.herokuapp.com';
 
 export const fetchCars = () => async (dispatch) => {
   const response = await axios.get(`${urlBase}/api/v1/cars`);
