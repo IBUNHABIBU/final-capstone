81115f22cfd0dcbd563c3e7b2ba6f0fc8ab99581
IBUNHABIBU <ibunhabib@gmail.com>
2022-07-26 00:04:01 +0300

    Change image_url to camel case

--- src/containers/NameList.js
+++ src/containers/NameList.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const NameList = ({ car }) => {
-  console.log(car);
   const {
     id, color, engine, imageUrl,
   } = car;
