b983fe4c351834dab376eff0e168d20d9cad024d
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-18 21:41:23 +0300

    Fix the appointment to remove duplication

--- src/redux/reducers/bookingReducer.js
+++ src/redux/reducers/bookingReducer.js
@@ -5,7 +5,7 @@ const initialState = [];
 const bookingReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case actionTypes.BOOK_CAR:
-      return [...state, ...payload];
+      return [...payload];
     default:
       return state;
   }
