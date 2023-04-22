14d9d97a105c0a6a08ec9cb854677ef154f75ace
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-22 22:31:02 +0300

    Add Nav

--- src/costants/index.js
+++ src/costants/index.js
@@ -1,1 +1,29 @@
-const lists = []
+const list = [
+    {
+      id: 1,
+      name: 'HOME',
+      path: '/',
+    },
+    {
+      id: 2,
+      name: 'MODELS',
+      path: '/models',
+    },
+    {
+      id: 3,
+      name: 'APPOINTMENTS',
+      path: '/appointments',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      path: '/contact',
+    },
+    {
+      id: 5,
+      name: 'Admin Panel',
+      path: '/admin',
+    },
+  ];
+  
+  export default list;
