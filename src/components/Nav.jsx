5cd451f8ced4f0b968781940919be9fbea6f42eb
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-22 22:36:48 +0300

    Add Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import { checkUser, urlBase } from '../redux/actions/fetch';
 import list from '../constants';
 import { setUser } from '../redux/actions/actions';
 
--- src/costants/index.js
+++ src/costants/index.js
@@ -1,19 +1,19 @@
 const list = [
-    {
-      id: 1,
-      name: 'HOME',
-      path: '/',
-    },
-    {
-      id: 2,
-      name: 'MODELS',
-      path: '/models',
-    },
-    {
-      id: 3,
-      name: 'APPOINTMENTS',
-      path: '/appointments',
-    },
-  ];
-  
-  export default list;
+  {
+    id: 1,
+    name: 'HOME',
+    path: '/',
+  },
+  {
+    id: 2,
+    name: 'MODELS',
+    path: '/models',
+  },
+  {
+    id: 3,
+    name: 'APPOINTMENTS',
+    path: '/appointments',
+  },
+];
+
+export default list;
