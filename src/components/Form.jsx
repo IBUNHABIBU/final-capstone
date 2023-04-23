bac36098f2b7dfb31c52ebee23245a014fb88671
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-23 23:26:44 +0300

    Add the form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = ({field, onSubmit}) => {
     const [formData, setFormData] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevState) => ({ ...prevState, [name]: value }))
+    }
   return (
     <div></div>
   )
