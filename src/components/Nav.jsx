6c11bfb4e619d325eaed37e66c2e95cd7dc56f77
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-23 11:52:41 +0300

    Style the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import { setUser } from '../redux/actions/actions';
 import list from '../costants';
@@ -8,6 +8,8 @@ import { urlBase } from '../redux/actions/fetch';
 
 const Nav = () => {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  console.log(user);
 
   const handleLogout = () => {
     axios.delete(`${urlBase}/logout`, { withCredentials: true })
@@ -36,25 +38,27 @@ const Nav = () => {
         }
         {
         user.logged_in ? (
-            <div>
-              <p>
-                <span className="p-2">Login as</span>
-                <strong>
-                  {user.name}
-                </strong>
-              </p>
-              <button type="submit" className="btn btn-primary col-4" onClick={handleLogout}>Logout</button>
-            </div>
-
-          ) :(<div className="user">
-          <NavLink to="/signup" className="link ">
-            <button type="submit" className="btn">Sinup</button>
-          </NavLink>
           <div>
-            <p>Already a member?</p>
-            <NavLink to="/login" className="link">Login</NavLink>
+            <p>
+              <span className="p-2">Login as</span>
+              <strong>
+                {user.name}
+              </strong>
+            </p>
+            <button type="submit" className="btn btn-primary col-4" onClick={handleLogout}>Logout</button>
           </div>
-        </div>)
+
+        ) : (
+          <div className="user">
+            <NavLink to="/signup" className="link ">
+              <button type="submit" className="btn">Sinup</button>
+            </NavLink>
+            <div>
+              <p>Already a member?</p>
+              <NavLink to="/login" className="link">Login</NavLink>
+            </div>
+          </div>
+        )
 }
       </div>
     </div>
