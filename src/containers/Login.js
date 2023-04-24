1ec86c1b585e429cc0a98bed64715d82a2b7a5e9
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-24 23:09:00 +0300

    Add login page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -32,8 +32,10 @@ const Login = () => {
     });
   };
   return (
-    <div className="login-page container">
-      <h1>Login</h1>
+    <div className="login">
+      <div className="login__header">
+        <h1>Login</h1>
+      </div>
       <Form
         field={
         [
