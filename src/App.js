b097ced4c13252f3a4933997b153751ae9686c5b
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-26 11:45:06 +0300

    Refactor the login page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Appointment from './containers/Appointment';
 import layout from './layouts';
 import './App.scss';
 import Pop from './containers/Pop';
+import Project from './containers/Project';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -23,6 +24,7 @@ const App = () => {
   const CarDetailsComponent = layout(CarDetails);
   const AppointmentComponent = layout(Appointment);
   const PopComponent = layout(Pop);
+  const ProjectComponent = layout(Project);
 
   useEffect(() => {
     dispatch(checkLoginStatus());
@@ -38,6 +40,7 @@ const App = () => {
       { path: '/cars/:id', element: <CarDetailsComponent /> },
       { path: '/appointments', element: <AppointmentComponent /> },
       { path: '/pop', element: <PopComponent /> },
+      { path: '/projects', element: <ProjectComponent />}
     ],
   );
   return (
--- src/containers/Pop.jsx
+++ src/containers/Pop.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Modal from '../components/Modal';
-import Project from './Project';
 
 const Pop = () => {
   const [showModal, setShowModal] = useState(false);
@@ -15,7 +14,6 @@ const Pop = () => {
 
   return (
     <div>
-      <Project />
       <button onClick={handleOpenModal} type="submit">Open Modal</button>
       <Modal onClose={handleCloseModal} title="My Modal" content="hello" />
       {showModal && (
