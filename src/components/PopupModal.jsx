2a7e62e9eaaf3d7b4cb8dac002e4888c014a5f35
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-26 22:55:55 +0300

    Refactor the add car form page

---
+++ src/components/PopupModal.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const PopupModal = ({ onClose, title, content }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(true);
+    onClose();
+  };
+
+  return (
+    <div className={`modal ${isOpen ? 'open' : ''}`}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <h1>Titlee</h1>
+          <h2>{title}</h2>
+          <button type="submit" className="close-btn" onClick={handleClose}>
+            &times;
+          </button>
+        </div>
+        <div className="modal-body">{content}</div>
+      </div>
+    </div>
+  );
+};
+
+PopupModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default PopupModal;
