798b3677260dff6086cf20da54f63d794fe17db4
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-28 15:12:49 +0300

    Change the files

---
+++ src/containers/Bookings.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import { useSelector } from 'react-redux';
+import uuid from 'react-uuid';
+
+const Bookings = () => {
+  const bookings = useSelector((state) => state.booking);
+  return (
+    <>
+      <p className="display-6">All appointment</p>
+      <table className="table table-light">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Client Name</th>
+            <th scope="col">Car Model</th>
+            <th scope="col">Pickup Date</th>
+            <th scope="col">Return Date</th>
+            <th scope="col">Location</th>
+          </tr>
+        </thead>
+        {
+         bookings.map((item) => {
+           const {
+             name, model, pickup, return_date, location,
+           } = item;
+           return (
+             <tbody key={uuid()}>
+               <tr key={uuid()}>
+                 <td>{name}</td>
+                 <td>{model}</td>
+                 <td>{pickup}</td>
+                 <td>{return_date}</td>
+                 <td>{location}</td>
+               </tr>
+             </tbody>
+           );
+         })
+        }
+      </table>
+    </>
+  );
+};
+
+export default Bookings;
