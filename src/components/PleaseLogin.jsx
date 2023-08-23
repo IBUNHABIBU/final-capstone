4fb98987f73954805dc2f75e9b709b9625cf2888
IBUNHABIBU <ibunhabib@gmail.com>
2023-08-23 17:31:40 +0300

    Add new file

--- src/components/PleaseLogin.jsx
+++ src/components/PleaseLogin.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const PleaseLogin = () => {
   return (
     <div className='authorize'>
-        
+        <div class="login-container">
+        <h1>Welcome to our Website</h1>
+        <p>Please log in to access the full content.</p>
+        <button class="login-button">Log In</button>
+    </div>
     </div>
   )
 }
