1f28ab51507a9768a8dc58c9340ced7b8ed9db4f
IBUNHABIBU <ibunhabib@gmail.com>
2023-08-23 19:38:08 +0300

    Style the authorize prompt page

--- src/components/PleaseLogin.jsx
+++ src/components/PleaseLogin.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const PleaseLogin = () => (
   <div className="authorize">
     <div className="login-container">
       <h1>Welcome to our Website</h1>
       <p>Please log in to access the full content.</p>
-      <button className="btn" type="button">Log In</button>
+      <NavLink to="/login" className="btn">Login</NavLink>
     </div>
   </div>
 );
