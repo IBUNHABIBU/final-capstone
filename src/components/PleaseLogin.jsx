ae5c7e1aab8cc9365a3c57dc17ed268df11a6fe3
IBUNHABIBU <ibunhabib@gmail.com>
2023-08-23 17:41:41 +0300

    Style the authorize prompt page

--- src/components/PleaseLogin.jsx
+++ src/components/PleaseLogin.jsx
@@ -5,7 +5,7 @@ const PleaseLogin = () => (
     <div className="login-container">
       <h1>Welcome to our Website</h1>
       <p>Please log in to access the full content.</p>
-      <button className="login-button" type="button">Log In</button>
+      <button className="btn" type="button">Log In</button>
     </div>
   </div>
 );
