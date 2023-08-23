2e29f5b37acb230f490a6c5c2b3a620c18144e13
IBUNHABIBU <ibunhabib@gmail.com>
2023-08-23 17:32:29 +0300

    Add new file

--- src/components/PleaseLogin.jsx
+++ src/components/PleaseLogin.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const PleaseLogin = () => {
-  return (
-    <div className='authorize'>
-        <div class="login-container">
-        <h1>Welcome to our Website</h1>
-        <p>Please log in to access the full content.</p>
-        <button class="login-button">Log In</button>
+const PleaseLogin = () => (
+  <div className="authorize">
+    <div className="login-container">
+      <h1>Welcome to our Website</h1>
+      <p>Please log in to access the full content.</p>
+      <button className="login-button" type="button">Log In</button>
     </div>
-    </div>
-  )
-}
+  </div>
+);
 
 export default PleaseLogin;
