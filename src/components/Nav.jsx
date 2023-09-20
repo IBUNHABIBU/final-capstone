12cc25b05084dd732032b5feeeef0ecd531dd240
IBUNHABIBU <ibunhabib@gmail.com>
2023-09-20 21:56:58 +0300

    Add humberger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,52 +21,53 @@ const Nav = () => {
 
   return (
     <div className="nav">
-      <div className="menu-btn">
-        <button type="button" className="btn btn--menu">
-          <FontAwesomeIcon icon={faBars} className="social--icon" />
-        </button>
-      </div>
-      <NavLink to="/" className="link">
-        <div className="nav__logo">
-          <img src="logo.jpg" alt="nav logo" />
+      <div className="nav__toggle">
+          <button type="button" className="btn btn--menu">
+            <FontAwesomeIcon icon={faBars} className="social--icon" />
+          </button>
         </div>
-      </NavLink>
-      <div className="nav__lists">
-        {
-        list.map((list) => (
-          <NavLink
-            to={list.path}
-            key={list.id}
-            className={({ isActive }) => (isActive ? 'link link--active' : 'link')}
-          >
-            {list.name}
-          </NavLink>
-        ))
-        }
-        {
-        user.logged_in ? (
-          <div className="user">
-            <p>
-              <span className="p-2">Login as</span>
-              <strong>
-                {user.details.name}
-              </strong>
-            </p>
-            <button type="submit" className="btn" onClick={handleLogout}>Logout</button>
+      <div className="nav__container">
+        <NavLink to="/" className="link">
+          <div className="nav__logo">
+            <img src="logo.jpg" alt="nav logo" />
           </div>
-
-        ) : (
-          <div className="user">
-            <NavLink to="/signup" className="link ">
-              <button type="submit" className="btn btn--signup">Sign Up</button>
+        </NavLink>
+        <div className="nav__lists">
+          {
+          list.map((list) => (
+            <NavLink
+              to={list.path}
+              key={list.id}
+              className={({ isActive }) => (isActive ? 'link link--active' : 'link')}
+            >
+              {list.name}
             </NavLink>
-            <div className="user__member">
-              <p>Already a member?</p>
-              <NavLink to="/login" className="user__member__link">Login</NavLink>
+          ))
+          }
+          {
+          user.logged_in ? (
+            <div className="user">
+              <p>
+                <span className="p-2">Login as</span>
+                <strong>
+                  {user.details.name}
+                </strong>
+              </p>
+              <button type="submit" className="btn" onClick={handleLogout}>Logout</button>
             </div>
-          </div>
-        )
-}
+          ) : (
+            <div className="user">
+              <NavLink to="/signup" className="link ">
+                <button type="submit" className="btn btn--signup">Sign Up</button>
+              </NavLink>
+              <div className="user__member">
+                <p>Already a member?</p>
+                <NavLink to="/login" className="user__member__link">Login</NavLink>
+              </div>
+            </div>
+          )
+        }
+        </div>
       </div>
     </div>
   );
