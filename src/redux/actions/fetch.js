f2feaca83fb38f8e19e2a6d27258ce75c1906776
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-28 01:13:51 +0300

    Fix errors

--- src/redux/actions/fetch.js
+++ src/redux/actions/fetch.js
@@ -46,7 +46,7 @@ export const checkLoginStatus = () => async (dispatch) => {
   const response = await axios.get(`${urlBase}/logged_in`, {
     withCredentials: true, mode: 'no-cors',
     timeout: 10000,
-  }).catch(error => console.error("there is Error", error));
+  }).ca
   dispatch(setUser(response.data));
 };
 
