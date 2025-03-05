803b32f07304fa4555662114e9ad28e8e0c08123
IBUNHABIBU <ibunhabib@gmail.com>
2025-03-05 15:25:24 +0300

    Fix error

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,6 +35,11 @@ const Nav = () => {
           <FontAwesomeIcon icon={faBars} className="social--icon" />
         </button>
       </div>
+      <NavLink to="/" className="link">
+          <div className="nav__logo">
+            <img src="logo.jpg" alt="nav logo" />
+          </div>
+      </NavLink>
       <div className={`nav__menu ${toggle ? '' : 'show'}`}>
         <div className="nav__lists">
           {
