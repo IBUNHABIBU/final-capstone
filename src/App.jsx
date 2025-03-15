5f3f1b2caf548e4e2da03466ad3f642cb04afe12
IBUNHABIBU <ibunhabib@gmail.com>
2025-03-15 12:25:35 +0300

    Remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
   const AppointmentComponent = layout(Appointment);
   const PopComponent = layout(Pop);
   const ProjectComponent = layout(Project);
-  const SliderComponent = layout(SliderMain);
+  // const SliderComponent = layout(SliderMain);
 
   useEffect(() => {
     dispatch(checkLoginStatus());
